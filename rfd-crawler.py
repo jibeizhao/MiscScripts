b5585fd65bdd74bc41f34f11c5ad3bb81200bcbb
jibei zhao <jibei.zhao@shopify.com>
2023-03-02 21:07:12 -0500

    V2 created by bing, with mysql

--- rfd-crawler.py
+++ rfd-crawler.py
@@ -1,33 +1,50 @@
-# Import the required libraries
 import requests
 from bs4 import BeautifulSoup
 
-# Make a request to the page URL and get the HTML content
+# connect to mysql database
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="yourdatabase"
+)
+mycursor = mydb.cursor()
+
+# create a table if not exists
+mycursor.execute("CREATE TABLE IF NOT EXISTS hot_deals (topictitle_retailer VARCHAR(255), title VARCHAR(255), post_voting VARCHAR(255))")
+
+# get the web page content
 url = "https://forums.redflagdeals.com/hot-deals-f9/"
 response = requests.get(url)
-html = response.text
+soup = BeautifulSoup(response.text, "html.parser")
 
-# Parse the HTML content using BeautifulSoup and find all the <li> tags with class "row topic"
-soup = BeautifulSoup(html, "html.parser")
-topics = soup.find_all("li", class_="row topic")
+# find all the div elements with class "thread_info_title"
+divs = soup.find_all("div", class_="thread_info_title")
 
-# Loop through each topic and extract the information you want
-for topic in topics:
-    # Get the value from the "topictitle_retailer" <a> tag
-    topictitle_retailer = topic.find("a", class_="topictitle_retailer").text
+# loop through each div element and extract the relevant information
+for div in divs:
+  # get the topic title retailer
+  topictitle_retailer = div.find("span", class_="topictitle_retailer").text.strip()
 
-    # Get the value from the <a> tag right after the above <a>
-    title = topic.find("a", class_="topictitle_retailer").find_next_sibling("a").text
+  # get the title and the link
+  a = div.find("a", class_="topic_title_link")
+  title = a.text.strip()
 
-    # Get the value from the  "total_count total_count_selector" <dd> tag
-    post_voting = topic.find("dd", class_="total_count total_count_selector").text
+  # get the post voting score
+  post_voting = div.find_next_sibling("div", class_="post_voting").text.strip()
 
-    # Create an object with these properties
-    obj = {
-        "topictitle_retailer": topictitle_retailer,
-        "title": title,
-        "post_voting": post_voting
-    }
+  # create an object with the extracted information
+  obj = {
+    "topictitle_retailer": topictitle_retailer,
+    "title": title,
+    "post_voting": post_voting,
+  }
 
-    # Do something with this object, such as print it or store it in a list or database
-    print(obj)
+  # insert the object into the mysql database table
+  sql = "INSERT INTO hot_deals (topictitle_retailer, title, post_voting) VALUES (%s, %s, %s)"
+  val = (obj["topictitle_retailer"], obj["title"], obj["post_voting"])
+  mycursor.execute(sql, val)
+
+# commit the changes to the database
+mydb.commit()
